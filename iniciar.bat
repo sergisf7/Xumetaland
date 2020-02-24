66722f8e38e1d9073426bccc198dba75a2dc04c4
Sergi Soria <sergisf9@gmail.com>
2020-02-24 17:05:35 +0100

    Ultima versio

--- iniciar.bat
+++ iniciar.bat
@@ -1,8 +1,9 @@
-git pull origin master
-pause
+echo Benvingut al launcher del millor servidor provehidor
+git fetch --all
+git reset --hard origin/master
+pause>nul|set/p =Pulsa enter per encendre lo server
 java -Xmx6G -jar mods.jar nogui
-pause
+pause>nul|set/p =Pulsa enter guardar els canvis
 git commit -am "Ultima versio"
-pause
 git push origin master
-pause
+pause>nul|set/p =Adeu siau
