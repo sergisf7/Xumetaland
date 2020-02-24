130f427e83677e8dab42f5713dbb5a902c2a2b8f
Sergi Soria <sergisf9@gmail.com>
2020-02-24 17:14:37 +0100

    Ultima versio

--- iniciar.bat
+++ iniciar.bat
@@ -2,7 +2,7 @@ echo Benvingut al launcher del millor servidor provehidor
 git fetch --all
 git reset --hard origin/master
 pause>nul|set/p =Pulsa enter per encendre lo server
-java -Xmx6G -jar mods.jar nogui
+java -jar mods.jar nogui
 pause>nul|set/p =Pulsa enter guardar els canvis
 git commit -am "Ultima versio"
 git push origin master
