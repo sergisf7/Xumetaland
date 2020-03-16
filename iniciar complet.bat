f1dd6d8c302b9a51fd7edd58011708566643b0e8
Sergi Soria <sergisf9@gmail.com>
2020-03-16 03:00:49 +0100

    Versio 16/03/2020- 3:00:48,95

--- iniciar complet.bat
+++ iniciar complet.bat
@@ -5,6 +5,6 @@ pause>nul|set/p =Pulsa enter per encendre lo server
 java -jar mods.jar nogui
 pause>nul|set/p =Pulsa enter guardar els canvis
 git add .
-git commit -am "Versio"
+git commit -am "Versio %date%-%time%"
 git push origin master
 pause>nul|set/p =Adeu siau
