947a51c65c8c2edf26f8b3b01d8f734e360116ef
Sergi Soria <sergisf9@gmail.com>
2020-03-16 02:37:25 +0100

    Ultima versio

--- iniciar complet.bat
+++ iniciar complet.bat
@@ -5,6 +5,6 @@ pause>nul|set/p =Pulsa enter per encendre lo server
 java -jar mods.jar nogui
 pause>nul|set/p =Pulsa enter guardar els canvis
 git add .
-git commit -am "Ultima versio"
+git commit -am "Versio `date +'%Y-%m-%d %H:%M:%S'`"
 git push origin master
 pause>nul|set/p =Adeu siau
