36ed68103af353eb43b9ff1d700c45537a946ed3
Sergi Soria <sergisf9@gmail.com>
2020-04-04 19:25:00 +0200

    Versio 04/04/2020-19:25:00,13

--- iniciar complet.bat
+++ iniciar complet.bat
@@ -2,7 +2,7 @@ echo Benvingut al launcher del millor servidor provehidor
 git fetch --all
 git reset --hard origin/master
 pause>nul|set/p =Pulsa enter per encendre lo server
-java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=30 -XX:G1ReservePercent=30 -XX:MaxGCPauseMillis=80 -XX:+DisableExplicitGC -XX:G1HeapRegionSize=32M -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -jar mods.jar nogui
+java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar mods.jar nogui
 pause>nul|set/p =Pulsa enter guardar els canvis
 git add .
 git rm -r logs
