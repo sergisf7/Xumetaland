437bfa2065b48bf98f56e47903917bfb8b311494
Sergi Soria <sergisf9@gmail.com>
2020-04-06 04:27:36 +0200

    Versio 06/04/2020- 4:27:35,99

--- iniciar.bat
+++ iniciar.bat
@@ -1,3 +1,3 @@
 pause>nul|set/p =Pulsa enter per encendre lo server
-java -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=30 -XX:G1ReservePercent=30 -XX:MaxGCPauseMillis=80 -XX:+DisableExplicitGC -XX:G1HeapRegionSize=32M -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -jar mods.jar nogui
+java -Dfml.queryResult=confirm -jar mods.jar nogui
 pause>nul|set/p =Adeu siau
